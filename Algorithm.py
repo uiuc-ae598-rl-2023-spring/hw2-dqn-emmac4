699f486d8861edb4214bfa5146aba3d07313f47e
emmac4 <83370499+emmac4@users.noreply.github.com>
2023-04-04 01:48:19 -0400

    Add files via upload

--- Algorithm.py
+++ Algorithm.py
@@ -11,6 +11,8 @@ import random
 from collections import namedtuple, deque
 import matplotlib.pyplot as plt 
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 Transition = namedtuple('Transition',
                 ('state', 'action', 'next_state', 'reward', 'dones'))
 class Policy(nn.Module): 
@@ -20,13 +22,11 @@ class Policy(nn.Module):
                                     nn.Tanh(),
                                     nn.Linear(64,64),
                                     nn.Tanh(),
-                                    nn.Linear(64,64), 
                                     nn.Linear(64, output_dim)) 
 
         return 
     
     def forward(self, x):
-    
         out = self.layers(x)
         return out
     
@@ -48,36 +48,54 @@ class Replay():
     
 class DQN(): 
     def __init__(self, env, gamma, lr, eps, batch, iters, use_memory, update_every): 
+        #discount factor
         self.gamma = gamma
+        #learn rate
         self.learn_rate = lr
-        
+        #for epsilon greedy action selection and epsilon decay 
         self.eps_start = eps 
-        self.min_eps = 0.1
+        self.eps = self.eps_start
+        self.min_eps = 0.0
         
+        #training hyperparameters
         self.iters = iters
         self.batch_size = batch 
         self.use_memory = use_memory 
         self.update = update_every 
         
+        #environment information 
         self.env = env
-        
         self.act_space = env.num_actions 
         self.obs_space = env.num_states
         
+        #initialize policy and target networks 
+        self.policy = Policy(self.obs_space, self.act_space)
+        self.target = Policy(self.obs_space, self.act_space)
+        #copy parameters of policy to the target network 
+        self.target.load_state_dict(self.policy.state_dict())
+        self.target.eval()
+        
+        #initialize replay buffer 
+        if self.use_memory == True: 
+            self.memory = Replay(10000)
+        else: 
+            #if buffer not in use, set max size = to batch size
+            self.memory = Replay(self.batch_size)
+        
+        #optimizer and loss function 
+        self.opt = torch.optim.Adam(self.policy.parameters(), lr = self.learn_rate)
+        self.loss_fun = nn.MSELoss()
         
         return 
     
-    def train_plots(self, loss, step, eval_rew, eval_step): 
-        steps = np.linspace(0, step, step)
-        fig, ax = plt.subplots()
-        ax.plot(eval_step, eval_rew, color = 'orange')
-        ax.set_ylim([-25, 25])
-        ax.set_ylabel('return')
-        ax.set_xlabel('steps')
-        
-        return 
+    def get_action(self, state): 
+        #get action from policy network 
+        with torch.no_grad(): 
+            action = self.policy(state).argmax(1)
+        return action.item()
     
     def greedy_action(self, s): 
+        #epsilon greedy action selection 
         with torch.no_grad():
             p = np.random.random()
             
@@ -85,129 +103,89 @@ class DQN():
                 a = random.randrange(self.act_space)
                 
             else: 
-                d = self.policy(torch.tensor(s).float())
-                a = torch.argmax(d).numpy().item()
-                
+                a = self.get_action(s)
+
         return a
     
-    def rollout(self, num): 
-        with torch.no_grad():
-            rollouts = deque([], maxlen=num)
-            for i in range(num): 
-                s = self.env.reset()
-                done = False
-                episode = deque([])
-                while not done: 
-                    a = self.greedy_action(s)
-                    s_new, r, done = self.env.step(a)
-                    episode = Transition(s, a, s_new, r, done)
-                    self.memory.store(episode)
-                        
-                    s = s_new
-                    
-                    rollouts.append(episode)
-        return rollouts
     
-    def evaluation(self): 
-        with torch.no_grad():
-            s = self.env.reset()
-            done = False
-            returns = 0
-            while not done: 
-                a = torch.argmax(self.policy(torch.tensor(s).float()))
-                s_new, r, done = self.env.step(a)
-                returns += r
-                s = s_new
-            
-        return returns
-    
+    def update_model(self):
+        #function to update the network model 
+        #sample batch from replay buffer
+        batch = self.memory.sample(self.batch_size)
+        batch = Transition(*zip(*batch))
+        
+        states = torch.cat([s.to(device) for s in batch.state])
+        new_states = torch.cat([ns.to(device) for ns in batch.next_state])
+        actions = torch.cat([a.to(device) for a in batch.action])
+        rewards = torch.cat([r.to(device) for r in batch.reward])
+        dones = torch.tensor(batch.dones).float()
+        
+        #calculate state-action values of current state
+        Q = self.policy(states).gather(1, actions.unsqueeze(1))
+        
+        #calculate expected values of next states using target network 
+        next_values = (1-dones)*self.target(new_states).max(1)[0].detach()
+        Q_tar = rewards + self.gamma*(next_values)
+        
+        loss = self.loss_fun(Q, Q_tar.unsqueeze(1))
+        self.opt.zero_grad()    
+        loss.backward()
+        torch.nn.utils.clip_grad_value_(self.policy.parameters(), 1)
+        self.opt.step()
+        
+        
+
     def train(self):
-        self.policy = Policy(self.obs_space, self.act_space).float()
-        self.target = Policy(self.obs_space, self.act_space).float()
-        
-        if self.use_memory == True: 
-            self.memory = Replay(10000)
-        else: 
-            self.memory = Replay(self.batch_size)
-            
-        self.eps = self.eps_start 
-        
-        opt = torch.optim.AdamW(self.policy.parameters(), lr = self.learn_rate, amsgrad= True)
-        #opt = torch.optim.RMSprop(self.policy.parameters(), lr = self.learn_rate)
-        loss_fun = nn.MSELoss()
+        train_return = []
+
         step = 0
-        eval_return = []
+        for i in range(self.iters):  
+            s = self.env.reset()
+            episode = deque([])
+            s = torch.tensor(s).float().unsqueeze(0)
+            ep_step = 0
+            train_rew = 0
+            for j in range(self.env.max_num_steps): 
+                #step through environment 
+                a = self.greedy_action(s)
+                s_new, r, done = self.env.step(a)
+                s_new = torch.tensor(s_new).float().unsqueeze(0)
+                r  = torch.tensor([r]).float()
+                train_rew += r.item()
+                a = torch.tensor([a])
+                
+                #store transition in replay buffer 
+                t = Transition(s, a, s_new, r, done)
+                self.memory.store(t)
+                s = s_new
+                
+                
+                if len(self.memory.memory) < self.batch_size: 
+                    #if there are not enough samples in memory, pass 
+                    pass
+                else: 
+                    self.update_model()
+                                
+                if done: 
+                    break
+                
+            train_return.append(train_rew)
+            
+            
+            #linear epsilon decay 
+            self.eps =np.max([self.min_eps, ((self.min_eps - self.eps_start)/(self.iters*.95))*i + self.eps_start])
         
-        for i in range(self.iters):
-            #get rollouts
-            rollout = self.rollout(self.batch_size) 
-             
-            
-            batch = self.memory.sample(self.batch_size)
-            batch = Transition(*zip(*batch))
-            
-            states = torch.tensor(batch.state).float()
-            new_states = torch.tensor(batch.next_state).float()
-            rewards = torch.tensor(batch.reward).float()
-            dones = torch.tensor(batch.dones)
-            actions = torch.tensor(batch.action)
-            
-            q = self.policy(states).gather(1, actions.reshape(actions.shape[0], 1))
-            
-            est_value = self.gamma*(self.target(new_states)).max(1)[0]
-            
-            #for terminal states, replace \gamma*Q_tar w/ 0 
-            with torch.no_grad():
-                for k in range(len(dones)): 
-                    if dones[k] == True :
-                        est_value[k] = 0 
-            
-            q_tar = rewards + est_value
-            
-            opt.zero_grad()
-            loss = loss_fun( q.reshape(q.shape[0], 1), q_tar.reshape(q_tar.shape[0], 1))
-            loss.backward()
-            torch.nn.utils.clip_grad_value_(self.policy.parameters(), 100)
-            opt.step()
-            step += 1 
-            
-            if (i+1) % self.update == 0: 
+            if i % self.update == 0: 
+                #copy policy parameters to target network 
                 self.target.load_state_dict(self.policy.state_dict())
-            
-            eval_rew = self.evaluation()
-            eval_return.append(np.sum(eval_rew))
-            
-            if (i+1) % 5 == 0: 
-                self.eps = np.max([self.eps*.99, self.min_eps])
-                print("training iteration: " + str(i))
-                print('epsilon: ' + str(self.eps))
-            
-        return self.policy,  eval_return, step
-        
-    def train_avg(self): 
-        _,  er1, s1 = self.train()
-        _,  er2, s2 = self.train()
-        _, er3, s3 = self.train()
-     
-        er = np.stack([er1, er2, er3]) 
-        er_avg = np.mean(er, axis = 0)
-        er_std = np.std(er, axis = 0)
-        er = np.stack([er_avg, er_std])
-        
-        step = s3
-        return  er, step 
-    
-    def train_plot_avg(self): 
-        er, es = self.train_avg() 
-        
-        s = np.linspace(0, es, es)
+        '''        
         fig, ax = plt.subplots()
-        ax.plot(s, er[0])
-        ax.fill_between(s, (er[0]-er[1]), (er[0]+er[1]), alpha = .25)
-        ax.set_ylabel('return')
-        ax.set_xlabel('iteration')
-        ax.set_ylim([-200, 200])
+        it = np.linspace(0, self.iters, self.iters)
+        ax.plot(it, train_return)
+        ax.set_ylim([-10, 100])
+        '''
+        return self.policy,  train_return
         
-        fig.savefig('./figures/learning_curve.png')
-        
+
     
+   
